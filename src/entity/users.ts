ff73d1729c290bc5d465257664cb4c626e9a3764
kleber <kleber.lo.pietro@gmail.com>
2026-08-24 19:41:51 -0300

    Third Commit

---
+++ src/entity/users.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, JoinColumn } from "typeorm"
+import { ManyToOne } from "typeorm/browser";
+import { Situation } from "./situations.js";
+
+@Entity("users")
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column()
+    nome!: string;
+
+    @Column({unique: true})
+    email!: string;
+
+    @ManyToOne(()=> Situation, (situation)=> situation.users)
+    @JoinColumn({name: "situation"})
+    situation!: Situation
+
+     @Column({type: "timestamp", default:()=>"CURRENT_TIMESTAMP"})
+    createdAt!: Date;
+
+    @Column({type: "timestamp", default:()=>"CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP"})
+    updatedAt!: Date;
+}
